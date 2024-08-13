4c46992391dbef78353d1578205c2a915259c6cc
jarrydms <dms8837@gmail.com>
2024-08-13 16:05:17 +0900

    code add 2day,ch02

---
+++ src/basic/OverflowDemo.java
@@ -0,0 +1,21 @@
+package basic;
+
+public class OverflowDemo {
+
+  public static void main(String[] args) {
+//
+//    int a = 1_000_000;
+//    int b = 2_000_000;
+//    long result =  (a * b);
+//    //a의 integer가 long 타입으로 변경된다.
+//    System.out.println(result);
+
+
+
+
+
+
+
+
+  }
+}
